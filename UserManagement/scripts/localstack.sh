b0edcb7acf549f6852011a3b32a5c968c7267245
Raphael <rapphaelmanhaes2017@hotmail.com>
2024-04-23 21:37:16 -0300

    Setup SQS in UserManagement

--- UserManagement/scripts/localstack.sh
+++ UserManagement/scripts/localstack.sh
@@ -1,5 +1,6 @@
-aws --endpoint http://localhost:4566 dynamodb create-table \
-  --table-name users \
+awslocal dynamodb create-table --table-name users \
   --attribute-definitions AttributeName=id,AttributeType=S AttributeName=email,AttributeType=S\
   --key-schema AttributeName=id,KeyType=HASH AttributeName=email,KeyType=RANGE \
   --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+
+awslocal sqs create-queue --queue-name bookings
