ad669f3a5b0f308123f4d99bc0f98256eaf383e5
Raphael <rapphaelmanhaes2017@hotmail.com>
2024-06-13 13:10:46 -0300

    Fix tests

--- UserManagement/src/test/java/com/raphaelcollin/usermanagement/infrastructure/IntegrationTest.java
+++ UserManagement/src/test/java/com/raphaelcollin/usermanagement/infrastructure/IntegrationTest.java
@@ -2,6 +2,7 @@ package com.raphaelcollin.usermanagement.infrastructure;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
