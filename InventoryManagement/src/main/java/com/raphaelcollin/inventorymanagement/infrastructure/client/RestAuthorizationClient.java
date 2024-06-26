6704a217ed5a005b134f68434300c6b2d4094679
Raphael <rapphaelmanhaes2017@hotmail.com>
2024-06-26 08:44:30 -0300

    Add url to RestAuthorizationClient

--- InventoryManagement/src/main/java/com/raphaelcollin/inventorymanagement/infrastructure/client/RestAuthorizationClient.java
+++ InventoryManagement/src/main/java/com/raphaelcollin/inventorymanagement/infrastructure/client/RestAuthorizationClient.java
@@ -2,19 +2,23 @@ package com.raphaelcollin.inventorymanagement.infrastructure.client;
 
 import com.raphaelcollin.inventorymanagement.core.exception.InvalidTokenException;
 import com.raphaelcollin.inventorymanagement.infrastructure.web.filter.AuthorizationClient;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Component
 public class RestAuthorizationClient implements AuthorizationClient {
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
+
+    @Value("${auth.authorizationserver.url}")
+    private String endpoint;
 
     @Override
     public void validateToken(String token) {
-        ResponseEntity<Void> response = restTemplate.postForEntity("http://localhost:8080/auth/validate", token, Void.class);
+        ResponseEntity<Void> response = restTemplate.postForEntity(endpoint, token, Void.class);
         if (response.getStatusCode().isError()) {
             throw new InvalidTokenException("Invalid token");
         }
